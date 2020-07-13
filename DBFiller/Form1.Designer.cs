ec3543a8ace14d52933bde21c5db782f72139169
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-07-13 16:41:13 +0300

    DBFiller implemented, needs to be tested

--- DBFiller/Form1.Designer.cs
+++ DBFiller/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileNameTB = new System.Windows.Forms.TextBox();
+            this.pictureNameTB = new System.Windows.Forms.TextBox();
+            this.pictureDescTB = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Назва картини та автор";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Опис картини";
+            // 
+            // fileNameTB
+            // 
+            this.fileNameTB.Location = new System.Drawing.Point(178, 33);
+            this.fileNameTB.Name = "fileNameTB";
+            this.fileNameTB.Size = new System.Drawing.Size(331, 20);
+            this.fileNameTB.TabIndex = 3;
+            // 
+            // pictureNameTB
+            // 
+            this.pictureNameTB.Location = new System.Drawing.Point(178, 90);
+            this.pictureNameTB.Name = "pictureNameTB";
+            this.pictureNameTB.Size = new System.Drawing.Size(331, 20);
+            this.pictureNameTB.TabIndex = 4;
+            // 
+            // pictureDescTB
+            // 
+            this.pictureDescTB.Location = new System.Drawing.Point(178, 151);
+            this.pictureDescTB.Name = "pictureDescTB";
+            this.pictureDescTB.Size = new System.Drawing.Size(331, 171);
+            this.pictureDescTB.TabIndex = 5;
+            this.pictureDescTB.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(525, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Зберегти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(664, 352);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureDescTB);
+            this.Controls.Add(this.pictureNameTB);
+            this.Controls.Add(this.fileNameTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fileNameTB;
+        private System.Windows.Forms.TextBox pictureNameTB;
+        private System.Windows.Forms.RichTextBox pictureDescTB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DBFiller/Form1.cs
+++ DBFiller/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,53 @@ namespace DBFiller
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static byte[] imageData;
+        private static string shortFileName;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            fileNameTB.Text = filename;
+
+            shortFileName = filename.Substring(filename.LastIndexOf('\\') + 1);
+
+
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                imageData = new byte[fs.Length];
+                fs.Read(imageData, 0, imageData.Length);
+            }
+        }
+
+
+        private const string connectionString = @"Host=localhost;Port=5432;Database=paintingsdb;Username=postgres;Password=enderant007A";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = @"INSERT INTO Paintings VALUES (@FileName, @PictureName, @Description, @ImageData)";
+                command.Parameters.Add("@FileName", SqlDbType.NVarChar, 50);
+                command.Parameters.Add("@PictureName", SqlDbType.NVarChar, 50);
+                command.Parameters.Add("@FileDesc", SqlDbType.NVarChar, 10000);
+                command.Parameters.Add("@Image", SqlDbType.Image, 100000000);
+
+
+                // передаем данные в команду через параметры
+                command.Parameters["@FileName"].Value = shortFileName;
+                command.Parameters["@PictureName"].Value = pictureNameTB.Text;
+                command.Parameters["@FileDesc"].Value = pictureDescTB.Text;
+                command.Parameters["@Image"].Value = imageData;
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
