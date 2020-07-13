f1262f1c424ebb6801a4fad47b604fce583c8aa5
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-07-13 16:00:07 +0300

    db needs to be filled

--- coreGalery/Controllers/HomeController.cs
+++ coreGalery/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using coreGalery.Models;
 
 namespace coreGalery.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private GaleryDbContext context;
+        public HomeController(GaleryDbContext context)
         {
-            return View();
+            this.context = context;
+        }
+
+        public ViewResult Index()
+        {
+            return View(context.Paintings.ToList());
         }
     }
 }
