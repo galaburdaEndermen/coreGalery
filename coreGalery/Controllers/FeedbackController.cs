13448455347b281b74b326c85733147b427bbc0b
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-07-21 18:13:08 +0300

    feedback page added, needs implementation and styling

--- coreGalery/Controllers/FeedbackController.cs
+++ coreGalery/Controllers/FeedbackController.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using coreGalery.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coreGalery.Controllers
 {
     public class FeedbackController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public ViewResult Form()
+        {
+            return View(new LetterModel());
+        }
+
+        [HttpPost]
+        public IActionResult Form(LetterModel letter)
+        {
+            //обработать пісьмо
+            return RedirectToAction(nameof(Success));
+        }
+
+        [HttpGet]
+        public ViewResult Success()
         {
             return View();
         }
---
+++ coreGalery/Models/LetterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreGalery.Models
+{
+    public class LetterModel
+    {
+        [Required(ErrorMessage = "Вкажіть ім\'я")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Вкажіть тему листа")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Вкажіть свій e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Напишіть текст листа")]
+        public string Text { get; set; }
+    }
+}
