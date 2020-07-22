2c665d97e53ddd7f7172d727df72796525356193
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-07-22 16:06:56 +0300

    implementation done, site needs some styling and optimization

--- coreGalery/Controllers/FeedbackController.cs
+++ coreGalery/Controllers/FeedbackController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using coreGalery.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coreGalery.Controllers
@@ -18,7 +21,22 @@ namespace coreGalery.Controllers
         [HttpPost]
         public IActionResult Form(LetterModel letter)
         {
-            //обработать пісьмо
+            Task.Run(() =>
+            {
+                MailAddress from = new MailAddress("galaburda0test@gmail.com", "Mail from gallery");
+                MailAddress to = new MailAddress("tipa1488amerikos@gmail.com");
+                MailMessage m = new MailMessage(from, to);
+                m.Subject = letter.Subject;
+                m.Body = letter.Text + "\n\n\n" + letter.Email + "\t" + letter.Name;
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("galaburda0test@gmail.com", "test007A");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+            });
+
+
             return RedirectToAction(nameof(Success));
         }
 
